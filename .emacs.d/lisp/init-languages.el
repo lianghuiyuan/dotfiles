f258f803cac06438c24295c2aa9a9189f80e88fd
robertzhouxh <robertzhouxh@gmail.com>
2017-10-09 15:21:28 +0800

    add note for erlang:

--- .emacs.d/lisp/init-languages.el
+++ .emacs.d/lisp/init-languages.el
@@ -171,7 +171,7 @@
         (car (split-string (shell-command-to-string "hostname"))))))
 
 ;; now
-;        C-c C-z for starting the node ===> code:add_path(ebin/)
+;        C-c C-z for starting the node ===> code:add_pathz("项目 ebin 目录")
 ;        M-. when the cursor is on a function to show its definition
 ;        M-TAB to autocomplete
 
