29a568e0f523ce893bb5a595f5a303fbe3f0c559
robertzhouxh <robertzhouxh@gmail.com>
2018-04-27 11:37:40 +0800

    indent the lisp file

--- .emacs.d/lisp/init-plantform.el
+++ .emacs.d/lisp/init-plantform.el
@@ -6,74 +6,74 @@
 (when (system-is-mac)
   ;; refer to: http://azaleasays.com/2013/07/05/setting-up-mac-os-x-and-iterm2-for-emacs/
   ;; Switch the Cmd and Meta keys
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier nil)
+  (setq mac-option-key-is-meta nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil)
 
 
   ;; -----------------------------------------------------------------------------
   ;; setting font for mac system
   ;; -----------------------------------------------------------------------------
 
-;; font size
-(set-face-attribute 'default nil :height 140)
+  ;; font size
+  (set-face-attribute 'default nil :height 140)
 
-; Set default font
-;(set-face-attribute 'default nil
-;                    :family "Source Code Pro"
-;                    :height 140
-;                    :weight 'normal
-;                    :width 'normal)
-;(set-frame-font "Monaco:pixelsize=13")
-;(dolist (charset '(han kana symbol cjk-misc bopomofo))
-;  (set-fontset-font (frame-parameter nil 'font)
-;                    charset
-;                    (font-spec :family "Hiragino Sans GB" :size 13)
-;                    ))
-
-
-(if (executable-find "gls")
-  (progn
-    (setq insert-directory-program "gls")
-    (setq dired-listing-switches "-lFaGh1v --group-directories-first"))
-  (setq dired-listing-switches "-ahlF"))
-
-(defun copy-from-osx ()
-  "Handle copy/paste intelligently on osx."
-  (let ((pbpaste (purecopy "/usr/bin/pbpaste")))
-    (if (and (eq system-type 'darwin)
-             (file-exists-p pbpaste))
-      (let ((tramp-mode nil)
-            (default-directory "~"))
-        (shell-command-to-string pbpaste)))))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "/usr/bin/pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
-(setq interprogram-cut-function 'paste-to-osx
-      interprogram-paste-function 'copy-from-osx)
+  ; Set default font
+  ;(set-face-attribute 'default nil
+  ;                    :family "Source Code Pro"
+  ;                    :height 140
+  ;                    :weight 'normal
+  ;                    :width 'normal)
+  ;(set-frame-font "Monaco:pixelsize=13")
+  ;(dolist (charset '(han kana symbol cjk-misc bopomofo))
+  ;  (set-fontset-font (frame-parameter nil 'font)
+  ;                    charset
+  ;                    (font-spec :family "Hiragino Sans GB" :size 13)
+  ;                    ))
 
 
-;; Trash.
-(defun move-file-to-trash (file)
-  "Use `trash' to move FILE to the system trash.
-  When using Homebrew, install it using \"brew install trash\"."
-  (call-process (executable-find "trash")
-                nil 0 nil
-                file))
-  (setq trash-directory "~/.Trash/emacs")
-  (setq delete-by-moving-to-trash t)
-  (defun system-move-file-to-trash (file)
-    "Use \"trash\" to move FILE to the system trash.
+  (if (executable-find "gls")
+    (progn
+      (setq insert-directory-program "gls")
+      (setq dired-listing-switches "-lFaGh1v --group-directories-first"))
+    (setq dired-listing-switches "-ahlF"))
+
+  (defun copy-from-osx ()
+    "Handle copy/paste intelligently on osx."
+    (let ((pbpaste (purecopy "/usr/bin/pbpaste")))
+      (if (and (eq system-type 'darwin)
+               (file-exists-p pbpaste))
+        (let ((tramp-mode nil)
+              (default-directory "~"))
+          (shell-command-to-string pbpaste)))))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "/usr/bin/pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (setq interprogram-cut-function 'paste-to-osx
+        interprogram-paste-function 'copy-from-osx)
+
+
+  ;; Trash.
+  (defun move-file-to-trash (file)
+    "Use `trash' to move FILE to the system trash.
     When using Homebrew, install it using \"brew install trash\"."
     (call-process (executable-find "trash")
                   nil 0 nil
                   file))
-    )
+    (setq trash-directory "~/.Trash/emacs")
+    (setq delete-by-moving-to-trash t)
+    (defun system-move-file-to-trash (file)
+      "Use \"trash\" to move FILE to the system trash.
+      When using Homebrew, install it using \"brew install trash\"."
+      (call-process (executable-find "trash")
+                    nil 0 nil
+                    file))
+      )
 
 
 (when (system-is-linux)
