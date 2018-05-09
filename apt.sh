4c0ea45bfdfc5675574e29e65f8d8d576292cd6d
robertzhouxh <robertzhouxh@gmail.com>
2018-05-09 10:51:37 +0800

    add note for ibus-remine

--- apt.sh
+++ apt.sh
@@ -296,7 +296,7 @@ echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 cecho "!!! now, you can run ===> [ gnome-tweak-tool or unity-tweak-tool ] ===> in the terminal to change the theme and icon :-)" $green
 cecho "!!! now, you can run ===> [ start ] gui app to setup the apps[guake, albert ...] that should run after the system bootstrap :-)" $green
-cecho "!!! [ System Settings -> Text Entry ] add rime :-)" $green
+cecho "!!! [ System Settings -> Text Entry ] add rime and use [shift + command] to switch the input source:-)" $green
 cecho "!!! [ System Settings -> keyboard] adjust speed to fast :-)" $green
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
