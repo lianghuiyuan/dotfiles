adcf26924a9db0518c12e5b8de6293de5b79eae3
Mr.L <lianghuiyuan@126.com>
2019-04-21 18:03:51 +0800

    emacs add typescript-mode

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -2,6 +2,11 @@
                ("--author=Mr.L"))
  (magit-dispatch nil)
  (magit-log
+  ("-Gfix" "--color" "--decorate")
+  ("--color" "--decorate")
   ("--graph" "--color" "--decorate"))
+ (magit-log:-G "fix")
+ (magit-merge nil)
  (magit-push nil)
+ (magit-rebase nil)
  (magit:--author "Mr.L"))
